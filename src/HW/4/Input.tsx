63ae165ece183c2b6a5d47de1a55ace01499958b
Oleg010-web <nesteroff_oleg@mail.ru>
2024-06-21 23:56:50 +0300

    03-04-homeworks-done

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,14 +1,16 @@
 type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+	currentText: string // НУЖНО ПРОТИПИЗИРОВАТЬ
+	setCurrentText: (currentText: string) => void // НУЖНО ПРОТИПИЗИРОВАТЬ
 };
 
 export const Input = (props: InputPropsType) => {
-	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
+	const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
 		// НУЖНО ДОПИСАТЬ
+		let newValue = event.currentTarget.value;
+		props.setCurrentText(newValue)
 	};
 
-	// return (
-	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	// );
+	return (
+	  <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
+	);
 };
